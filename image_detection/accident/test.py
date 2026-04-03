b278c2f25ff750f71c0299b07d50f7d80a1686e3
Harsh Gupta <guptaharsh7714@gmail.com>
2026-04-03 14:04:31 +0530

    some file locations changed

--- image_detection/accident/test.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("accidents.pt")
-
-results = model("image.jpg")
-
-results[0].show()
--- image_detection/fire_smoke/test.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("fire_smoke.pt")
-
-print("Class Names:", model.names)
-print("Number of Classes:", len(model.names))
